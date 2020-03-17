0bc941af65e6e0f0a30610fa0dd148215fd8fb76
Alice (MarkelovNA) <alice@example.com>
2020-03-17 22:39:08 +0300

    code: format output

--- main.cpp
+++ main.cpp
@@ -9,10 +9,9 @@ int main()
     cin >> a >> b;
 
     cout << "A + B = " << a + b << '\n'
-     << "A - B = " << a - b << '\n';
-
-     cout << "A * B =" << a * b << '\n';
-     cout << "A / B =" << a / b << '\n';
+     << "A - B = " << a - b << '\n'
+     << "A * B = " << a * b << '\n'
+     << "A / B = " << a / b << '\n';
 
     return 0;
 }
